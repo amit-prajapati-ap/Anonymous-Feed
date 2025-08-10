85636436dfdd10ae966abf3c23cbf7a087092f14
amit-prajapati-ap <prajapatiapamit@gmail.com>
2025-08-10 19:19:09 +0530

    feat: add alert dialog component and integrate with message deletion
    - Added `@radix-ui/react-alert-dialog` dependency for alert dialog functionality.
    - Implemented `AlertDialog` component for confirmation before message deletion.
    - Created `MessageCard` component to display messages with delete functionality.
    - Added `Navbar` component for user authentication and navigation.
    - Implemented API route for deleting messages from the database.
    - Updated layout to include `Navbar` and toast notifications for user feedback.
    - Created reusable `Card` component for consistent UI presentation.
    - Enhanced button styles for better user interaction.

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,35 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import User from "@/model/User.model";
+import { User as NextAuthUser } from "next-auth";
+import mongoose from "mongoose";
+
+export async function DELETE(req: Request, {params}: {params: {messageid: string}}) {
+  const messageId = params.messageid
+  await dbConnect()
+
+  try {
+    const session = await getServerSession(authOptions)
+    const user: NextAuthUser = session?.user as NextAuthUser
+
+    console.log(session, user)
+
+    if (!session || !session.user) {
+      console.log("User not authenticated");
+      return Response.json({ success: false, message: "Unauthorized" }, { status: 401 })
+    }
+
+    const updateResult = await User.updateOne({_id: user._id}, {$pull: {messages: {_id: messageId}}})
+
+    if (updateResult.modifiedCount === 0) {
+      return Response.json({ success: false, message: "Message not found or already deleted" }, { status: 400 })
+    }
+
+    return Response.json({ success: true, message: "Message deleted successfully" }, { status: 200 })
+    
+  } catch (error) {
+    console.log("Error to deleting message :: ", error);
+    return Response.json({ success: false, message: "Failed to delete message" }, { status: 500 })
+  }
+}
